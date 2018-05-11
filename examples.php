c69fe8c13b9020a17210aeec271f51798af9911a
神楽坂覚々 <i@ttt.moe>
2018-05-11 10:51:43 +0800

    add examples

--- examples.php
+++ examples.php
@@ -1,10 +1,13 @@
 <?php
 include 'src/cURL.php';
 $curl = (new \Satori\cURL)->url("https://satori.moe/")
+    ->retry(3)
     ->post([
         'user_key' => "aaaaa",
     ])
     ->cookie(['id' => 2333])
+    ->timeout(4)
+    ->proxy("socks5://127.0.0.1:1080")
     ->header(['authorized_token' => time()])
     ->go();
 print_r($curl->info());
